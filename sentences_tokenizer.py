c6284e7d02543062382a386fdcda86b1f30d582d
galinaabdurashitova <g.m.abdurashitova@gmail.com>
2017-03-22 01:28:23 +0300

    tokenizer
    makes a list of sentences

---
+++ sentences_tokenizer.py
@@ -0,0 +1,32 @@
+# With no comments there's 25 lines :)
+
+
+# This function splits text for blocks that are probably sentences
+def lines_splitter(text):
+    for symbol in '.!?…':
+        text = text.replace(symbol + ' ', symbol)  # To escape empty elements
+        text = text.replace(symbol, '\n' + symbol + '\n')  # \n is a sign for separation
+    text = text.replace(': "', ':\n"')  # Some quotes can be sentences as well
+    return [line for line in text.split('\n') if line != '']  # Returns array of sentence-blocks and punctuation
+
+
+# This function makes text tokenized
+def tokenizer(text):
+    lines = lines_splitter(text)
+    capitals = 'ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТТУФХЦЧШЩЪЫЬЭЮЯ"'  # No RegEx to make the process faster
+    sentences, line = [], lines[0]
+    for i in range(len(lines) - 1):  # Looking at the pair of elements
+        if lines[i + 1][0] not in capitals:  # If the next element doesn't look like a beginning of a sentence
+            line = line + ' ' + lines[i + 1]  # It should be joined to the previous part
+        else:
+            sentences.append(line)
+            line = lines[i + 1]  # Otherwise remember the previous sentence and start the new one
+    return sentences
+
+
+if __name__ == '__main__':
+    with open('C:/Users/S451/Desktop/aa/long_poem.txt', 'r', encoding='utf-8') as f:
+        f = f.read()
+    tokens = tokenizer(f)
+#    for sentence in tokens:
+#        print(sentence)
